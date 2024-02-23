2aead113f6dd9900b80cd8505d30e6f2adee3911
MangaliSaiLakshman <157874940+MangaliSaiLakshman@users.noreply.github.com>
2024-02-23 07:41:52 +0530

    Delete Object Repository directory

--- Object Repository/parameters/login fields.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>login fields</name>
-   <tag></tag>
-   <elementGuidId>bc9323dc-8771-4cb4-981a-7a7da7fdc84c</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
